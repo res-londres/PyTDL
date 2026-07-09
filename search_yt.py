7a4cd26d6b9525f57181caa6f13276e0e77d7bb1
res-londres <jimuel.londres@gmail.com>
2026-07-09 17:50:32 +0800

    refactor: move search_yt logic into SearchYT class add SearchYT class to unify single and multiple searches mirror Download class

--- search_yt.py
+++ search_yt.py
@@ -1,23 +1,45 @@
+from concurrent.futures import ThreadPoolExecutor
+import itertools
 import yt_dlp
 
-def search_yt(query):
-    # returns info of top yt search result
-    ydl_opts = {
-        'default_search': 'ytsearch1:',
-        'noplaylist': True,
-        'quiet': True,
-    }
+class SearchYT:
+    @staticmethod
+    def search_yt(query, quiet=False):
+        # returns info of top yt search result
+        ydl_opts = {
+            'default_search': 'ytsearch1:',
+            'noplaylist': True,
+            'quiet': True,
+            'no_warnings': True,
+        }
 
-    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
-        result = ydl.extract_info(query, download=False)
-        top_result = result['entries'][0]
-        return top_result
+        with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+            try:
+                result = ydl.extract_info(query, download=False)
+            except:
+                if not quiet:
+                    print(f'No results for query "{query}"')
+                return dict()
+            if not quiet:
+                print(f'Found results for query "{query}"')
+            top_result = result['entries'][0]
+            return top_result
+
+    @staticmethod
+    def search_yt_mult(queries, quiet=False):
+        with ThreadPoolExecutor(max_workers=5) as executor:
+            results = executor.map(
+                SearchYT.search_yt, queries,
+                itertools.repeat(quiet)
+            )
+        return list(result for result in results if result)
 
 if __name__ == '__main__':
     # test
-    query = input('search for video: ')
-    result = search_yt(query)
-    print(result.get('webpage_url'))
+    with open('test', 'r') as f:
+        queries = [line.strip() for line in f if line.strip()]
+    search_yt = SearchYT
+    audio_metadata = search_yt.search_yt_mult(queries)
 
 
 
