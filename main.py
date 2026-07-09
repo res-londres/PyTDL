625f0d89f36b71d7c0b80f2dcc94c945cb9904d4
res-londres <jimuel.londres@gmail.com>
2026-07-09 13:20:06 +0800

    feat: add search_yt function, return top result metadata

---
+++ main.py
@@ -0,0 +1,2 @@
+from search_yt import search_yt
+
---
+++ search_yt.py
@@ -0,0 +1,25 @@
+import yt_dlp
+
+def search_yt(query):
+    # returns info of top yt search result
+    ydl_opts = {
+        'default_search': 'ytsearch1:',
+        'format': 'bestaudio/best',
+        'noplaylist': True,
+        'quiet': True,
+    }
+
+    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+        result = ydl.extract_info(query, download=False)
+        top_result = result['entries'][0]
+        print(top_result)
+        return top_result
+
+if __name__ == '__main__':
+    # test
+    query = input('search for video: ')
+    search_yt(query)
+
+
+
+
